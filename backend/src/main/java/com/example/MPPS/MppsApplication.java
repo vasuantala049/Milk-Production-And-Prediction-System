d094fd0bcd0cefd9f01bfff4f092ed75e6a1409f
Prithviraj009 <prithvirajsinhbarad05@gmail.com>
2025-12-13 19:40:27 +0530

    first commit

---
+++ backend/src/main/java/com/example/MPPS/MppsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MPPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/MPPS/MppsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MPPS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MppsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
