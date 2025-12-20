8e85c745861a58f948bbda7ecc440c98149146a4
ѕпоѡмап <darkphantomgamerz@gmail.com>
2025-12-20 12:16:10 +0530

    Update SecurityConfig.java

--- backend/backend/src/main/java/com/example/backend/Security/SecurityConfig.java
+++ backend/backend/src/main/java/com/example/backend/Security/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
 import java.util.List;
