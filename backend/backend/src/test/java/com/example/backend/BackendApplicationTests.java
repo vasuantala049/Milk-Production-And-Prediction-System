5a0527dd5928536db88941db9f44e7eae9e80690
ѕпоѡмап <darkphantomgamerz@gmail.com>
2025-12-20 11:55:38 +0530

    testcase created

--- backend/backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.backend;
 
+import com.example.backend.Controller.UserController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.backend.Controller.UserController;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BackendApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void shouldHitDbAndReturnOk() throws Exception {
+		mockMvc.perform(get("/api/test-db"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("DB OK"));
 	}
 
 }
