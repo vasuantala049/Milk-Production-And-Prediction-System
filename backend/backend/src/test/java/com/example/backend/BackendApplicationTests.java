771d6341ce1bedf66c46725e0f1d2b4fff708c5f
ѕпоѡмап <darkphantomgamerz@gmail.com>
2025-12-20 12:25:08 +0530

    Update BackendApplicationTests for web environment

--- backend/backend/src/test/java/com/example/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/example/backend/BackendApplicationTests.java
@@ -7,13 +7,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.example.backend.Controller.UserController;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-
+import org.springframework.test.context.ActiveProfiles;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @SpringBootTest
 @AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class BackendApplicationTests {
 	@Autowired
 	private MockMvc mockMvc;
