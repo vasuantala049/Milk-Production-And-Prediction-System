7060c420266cb2fe37a1115ff38ab52c7ff73ab2
Prithviraj009 <prithvirajsinhbarad05@gmail.com>
2026-01-06 20:53:55 +0530

    improve ui start

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,10 @@
 // );
 
 //new code for qr code support removed strict mode
+import '@fontsource/inter/300.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
